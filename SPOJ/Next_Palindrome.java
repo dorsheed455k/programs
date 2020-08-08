72ca8bce5af867a4b6a8cdeb928de86c921a617a
dorsheed455k <41130930+dorsheed455k@users.noreply.github.com>
2020-08-08 17:48:16 -0400

    Update Next_Palindrome.java

--- SPOJ/Next_Palindrome.java
+++ SPOJ/Next_Palindrome.java
@@ -1,40 +1,30 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Scanner in = new Scanner(System.in);
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(in.nextLine());
+        int n = Integer.parseInt(in.readLine());
 
         for (int i = 0; i < n; i++) {
-            int x = in.nextInt();
-
-            int[] count = new int[1000000-x];
-            for (int j = x; j < 1000000-x; j++) {
+           int x = Integer.parseInt(in.readLine());
+           int count;
+           int j = x;
+           while(true) {
+               j++;
                 String str = String.valueOf(j);
-                String rev = "";
-                for (int k = str.length()-1; k >= 0 ; k--) {
-                    rev = rev + str.charAt(k);
-                }
-                if(Integer.parseInt(rev) == Integer.parseInt(str)) {
-                    count[j] += Integer.parseInt(rev);
+                StringBuilder rev = new StringBuilder(str);
+                rev.reverse();
+                if(Integer.parseInt(rev.toString()) == Integer.parseInt(str)) {
+                    count = Integer.parseInt(rev.toString());
+                    System.out.println(count);
+                    break;
                 }
             }
-
-            System.out.println(minPalin(x, count));
         }
-    }
-
-    public static int minPalin(int x, int[]count) {
-        int min = Integer.MAX_VALUE;
-        for (int i : count) {
-            if (i > x && i < min) {
-                min = i;
-            }
-        }
-        return min;
     }
 }
