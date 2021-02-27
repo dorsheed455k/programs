de5c7b71427016ee88bd3a89d3212676823b919f
dorsheed455k <41130930+dorsheed455k@users.noreply.github.com>
2021-02-27 15:05:35 -0500

    Create CCC_21_J3.java

---
+++ DMOJ Solutions/CCC_21_J3.java
@@ -0,0 +1,28 @@
+/*
+  CCC_21_J3 - Boiling Water
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CCC_21_J3 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int b = Integer.parseInt(in.readLine());
+
+        int p = 5 * b - 400;
+
+        System.out.println(p);
+        if (p < 100) {
+            System.out.println(1);
+        } else if(p == 100) {
+            System.out.println(0);
+        } else {
+            System.out.println(-1);
+        }
+    }
+}
