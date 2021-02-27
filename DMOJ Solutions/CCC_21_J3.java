4475de3ca572c5d166b04ff3a2be725909a8180c
dorsheed455k <41130930+dorsheed455k@users.noreply.github.com>
2021-02-27 15:08:41 -0500

    Create CCC_21_J3.java

---
+++ DMOJ Solutions/CCC_21_J3.java
@@ -0,0 +1,39 @@
+/*
+  CCC_21_J3 - Secret_Instructions
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CCC_21_J3 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = in.readLine();
+        String prev = "";
+
+        while (!n.equals("99999")) {
+
+            int sum = (n.charAt(0) - '0') + (n.charAt(1) - '0');
+
+            if (sum % 2 == 1) {
+                System.out.print("left ");
+                prev = "left ";
+            } else if(sum % 2 == 0) {
+                if (sum != 0) {
+                    System.out.print("right ");
+                    prev = "right ";
+                } else {
+                    System.out.print(prev);
+                }
+            }
+            System.out.println(n.substring(2));
+
+            n = in.readLine();
+
+        }
+    }
+}
