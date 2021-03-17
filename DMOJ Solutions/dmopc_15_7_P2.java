ec918a6d8395409a29bd9bd02700044b6448a278
dorsheed455k <41130930+dorsheed455k@users.noreply.github.com>
2021-03-17 18:00:54 -0400

    Create dmopc_15_7_P2.java

---
+++ DMOJ Solutions/dmopc_15_7_P2.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class dmopc_15_7_P2 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] ln = in.readLine().split(" ");
+
+        System.out.println(ln.length);
+    }
+}
