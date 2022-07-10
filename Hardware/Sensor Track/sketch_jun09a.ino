5bb836f5c911aa9456ddda295a182c8c0f392da3
dorsheed455k <41130930+dorsheed455k@users.noreply.github.com>
2022-07-10 16:35:30 -0400

    Update sketch_jun09a.ino

--- Hardware/Sensor Track/sketch_jun09a.ino
+++ Hardware/Sensor Track/sketch_jun09a.ino
@@ -1,11 +1,28 @@
+#define echoPin 12
+#define trigPin 11
 
+long duration;
+int distance;
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ digitalWrite(trigPin, LOW);
+ delayMicroseconds(2);
+ 
+ digitalWrite(trigPin, HIGH);
+ delayMicroseconds(10);
+ digitalWrite(trigPin, LOW);
+
+ duration = pulseIn(echoPin, HIGH);
+ distance = duration * 0.034 / 2;
+
+ Serial.println(distance);
+ delay(100);
 
 }
